b4d36bb4808259b66862f4cbc449470c004503e4
Ronald Kinard <furyhunter600@gmail.com>
2016-05-03 00:17:39 -0500

    Update to 1.9.2, close recv stream, parse entity codes

--- src/main/java/com/teej107/slack/AsyncPlayerChatListener.java
+++ src/main/java/com/teej107/slack/AsyncPlayerChatListener.java
@@ -7,18 +7,15 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
  * @author teej107
  * @since Sep 12, 2015
  */
-public class AsyncPlayerChatListener implements Listener
-{
-	private Slack plugin;
+public class AsyncPlayerChatListener implements Listener {
+    private Slack plugin;
 
-	public AsyncPlayerChatListener(Slack plugin)
-	{
-		this.plugin = plugin;
-	}
+    public AsyncPlayerChatListener(Slack plugin) {
+        this.plugin = plugin;
+    }
 
-	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onAsyncChat(AsyncPlayerChatEvent event)
-	{
-		plugin.sendToSlack(event.getPlayer(), event.getMessage());
-	}
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onAsyncChat(AsyncPlayerChatEvent event) {
+        plugin.sendToSlack(event.getPlayer(), event.getMessage());
+    }
 }
--- src/main/java/com/teej107/slack/SlackReceiver.java
+++ src/main/java/com/teej107/slack/SlackReceiver.java
@@ -10,56 +10,50 @@ import java.util.concurrent.*;
  * @author teej107
  * @since Sep 12, 2015
  */
-public class SlackReceiver implements Runnable
-{
-	private URL url;
-	private ExecutorService service;
-	private Queue<String> queue;
+public class SlackReceiver implements Runnable {
+    private URL url;
+    private ExecutorService service;
+    private Queue<String> queue;
 
-	public SlackReceiver(URL url) throws IOException
-	{
-		this.url = url;
-		service = Executors.newSingleThreadExecutor();
-		queue = new ConcurrentLinkedQueue<>();
-	}
+    public SlackReceiver(URL url) throws IOException {
+        this.url = url;
+        service = Executors.newSingleThreadExecutor();
+        queue = new ConcurrentLinkedQueue<>();
+    }
 
-	public void send(String s)
-	{
-		queue.add(s);
-		service.submit(this);
-	}
+    public void send(String s) {
+        queue.add(s);
+        service.submit(this);
+    }
 
-	@Override
-	public void run()
-	{
-		String s = queue.poll();
-		if (s == null)
-			return;
+    @Override
+    public void run() {
+        String s = queue.poll();
+        if (s == null)
+            return;
 
-		try
-		{
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("POST");
-			connection.setConnectTimeout(5000);
-			connection.setUseCaches(false);
-			connection.setDoInput(true);
-			connection.setDoOutput(true);
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setConnectTimeout(5000);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
 
-			String payload = "payload="
-					+ URLEncoder.encode(s, "UTF-8");
+            String payload = "payload="
+                    + URLEncoder.encode(s, "UTF-8");
 
-			DataOutputStream wr = new DataOutputStream(
-					connection.getOutputStream());
-			wr.writeBytes(payload);
-			wr.flush();
+            DataOutputStream wr = new DataOutputStream(
+                    connection.getOutputStream());
+            wr.writeBytes(payload);
+            wr.flush();
+            wr.close();
 
-			//Seem useless but it isn't :P
-			connection.getInputStream();
-			connection.disconnect();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
+            //Seem useless but it isn't :P
+            connection.getInputStream().close();
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

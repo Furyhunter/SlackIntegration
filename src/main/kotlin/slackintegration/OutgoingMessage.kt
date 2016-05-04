857f2082a1590589179531591168dd71bc131c12
Ronald Kinard <furyhunter600@gmail.com>
2016-05-04 17:06:05 -0500

    Avatars

--- src/main/kotlin/slackintegration/OutgoingMessage.kt
+++ src/main/kotlin/slackintegration/OutgoingMessage.kt
@@ -6,7 +6,7 @@ interface OutgoingMessage {
 
 data class UserChatMessage(val user: String, val text: String, val channel: String) : OutgoingMessage {
     override val payload: String
-        get() = """{"username": "$user", "text": "$text", "channel": "$channel"}"""
+        get() = """{"username": "$user", "text": "$text", "channel": "$channel", "icon_url": "https://minotar.net/avatar/$user.png"}"""
 }
 
 data class EventMessage(val text: String, val channel: String) : OutgoingMessage {
